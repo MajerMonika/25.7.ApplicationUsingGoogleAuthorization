6bfa505f8b238592d75e0068257a2851028629cf
Monika Majer <majer_monika@interia.pl>
2018-12-28 12:10:11 +0100

    Project init, js code, pug files

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'GOOGLE_CLIENT_ID': '685779173150-oo7ce8afir6k7t7tkggt4udk8b0270vg.apps.googleusercontent.com',
+  'GOOGLE_CLIENT_SECRET': '4L3MrPto9MqCf6y9deMrsGeC',
+  'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
